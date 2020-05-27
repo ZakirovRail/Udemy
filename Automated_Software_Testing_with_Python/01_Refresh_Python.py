b3f693b531af4665660ead63ec81bde32305f970
R F <R@gmail.com>
2020-05-27 17:01:43 +0200

    First chapter of course - 28 videos completed

---
+++ Automated_Software_Testing_with_Python/01_Refresh_Python.py
@@ -0,0 +1,131 @@
+# name = 'Bob'
+# greeting = f"Hello, {name}"
+# print(greeting)  #  Hello, Bob
+#-------------------------------------------------------------------------------------------------
+# name = 'Rob'
+# greeting = "Hello, {}"
+# with_name = greeting.format(name)
+# print(with_name)  #  Hello, Rob
+# with_name_2 = greeting.format("Rolf")
+# print(with_name_2)  #  Hello, Rolf
+
+#-------------------------------------------------------------------------------------------------
+# longer_phrases = "Hello {}. Today is {}"
+# formated = longer_phrases.format("Ralf", "Monday")  #  Hello Ralf. Today is Monday
+# print(formated)
+
+
+
+#-------------------------------------------------------------------------------------------------
+# size_input = input("How big is your house (in square feet): ")
+# square_feet = int(size_input)
+# square_meters = square_feet / 10.8
+# print(f"{square_feet} square feet is {square_meters:.3f} square meters")
+
+#-------------------------------------------------------------------------------------------------
+
+# numbers = [1, 3, 5]
+# doubled = [i*2 for i in [1,3,5]]
+# print(doubled)
+
+# friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+# start_s = [i for i in friends if i.startswith('S')]
+# print(start_s)
+#
+#
+# start_s = []
+# for i in friends:
+#     if i.startswith('S'):
+#         start_s.append(i)
+# print(start_s)
+
+
+#-------------------------------------------------------------------------------------------------
+# student_attendance= {"Rolf":96, "Bob":80, "Anne":100}
+# for student in student_attendance:
+#     print(f"{student}: {student_attendance[student]}")
+#
+# print("===============================================")
+#
+# student_attendance= {"Rolf":96, "Bob":80, "Anne":100}
+# for student, attendance in student_attendance.items():
+#     print(f"{student}: {attendance}")
+
+#-------------------------------------------------------------------------------------------------
+
+# t = 5, 11
+# x, y = t
+# print(x, y)  #  5 11
+
+
+#-------------------------------------------------------------------------------------------------
+# people = [("Bob", 42, "Mechanic"), ("Anna", 21, "engineer"), ("Paul", 61, "Manager")]
+#
+# for name, age, profession in people:
+#     print(f"Name:{name}, Age:{age}, Profession:{profession}")
+
+
+
+#-------------------------------------------------------------------------------------------------
+# head, *tail = [1,2,3,4,5]
+# print(head)
+# print(tail)
+#
+# *head, tail = [1,2,3,4,5]
+# print(head)
+# print(tail)
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
